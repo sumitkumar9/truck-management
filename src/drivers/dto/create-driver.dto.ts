c5dcafd6d8b934d762051842cbe37817a46e402b
sumitkumar9 <sumit.ank0@gmail.com>
2025-07-20 08:58:55 +0530

    dto added

--- src/drivers/dto/create-driver.dto.ts
+++ src/drivers/dto/create-driver.dto.ts
@@ -1,1 +1,25 @@
-export class CreateDriverDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateDriverDto {
+  @IsString()
+  @IsNotEmpty()
+  first_name: string;
+
+  @IsString()
+  last_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  license_number: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsBoolean()
+  is_active: boolean;
+
+  @IsNumber()
+  created_by: number;
+}
